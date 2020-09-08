8712ee156b59a4fe92e78131aaba3cde13165f92
Joshua Iceton <Iceto.J01@elliotthudsoncollege.ac.uk>
2020-09-08 14:14:16 +0100

    End of Task 2
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

---
+++ Assets/Scripts/CheckDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CheckDeath : MonoBehaviour {
+
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "deathZone")
+	{
+			Debug.Log("I collided with the " + col.gameObject.tag);
+			//Destroy (gameObject);
+			//re-loads the current scene
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+	}
+
+
+
+		
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "rightwall"){
+			Debug.Log ("I collided with the " + col.gameObject.tag);
+		}
+		else if (col.gameObject.tag == "leftWall")
+		{
+			Debug.Log ("I collided with the " + col.gameObject.tag);
+		}
+
+		else if (col.gameObject.tag == "floor") {
+				Debug.Log ("I collided with the " + col.gameObject.tag);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (new Vector3 (1, 0, 0), ForceMode.Impulse); }
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (new Vector3 (-1, 0, 0), ForceMode.Impulse); }
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (new Vector3 (0, 5, 0), ForceMode.Impulse);
+			Debug.Log ("Jump Jump!"); }
+	}
+		
+		
+
+}
