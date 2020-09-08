793d6044d4caf5ecbc2d82f3adbd4f9e7faf2e1a
Joshua Iceton <Iceto.J01@elliotthudsoncollege.ac.uk>
2020-09-08 14:37:04 +0100

    End of Task 3
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

---
+++ Assets/Scripts/AddPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPoints : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "collectible5pt") {
+			Debug.Log ("I collected 5 points");
+			Destroy (col.gameObject);
+		}
+	
+}
+
+
+
+}
