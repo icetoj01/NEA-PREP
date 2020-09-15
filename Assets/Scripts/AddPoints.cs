883c425d6545cc8a185630bd1b1d4d8594d71608
Joshua Iceton <Iceto.J01@elliotthudsoncollege.ac.uk>
2020-09-15 09:35:26 +0100

    End of Task 4
    task4
    
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class AddPoints : MonoBehaviour {
+
+	private int score;
+	public TextMeshPro scoreText;
 
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.tag == "collectible5pt") {
 			Debug.Log ("I collected 5 points");
 			Destroy (col.gameObject);
+			score += 5;
+
+			scoreText.text = "Score: " + score.ToString ();
+		} else if (col.gameObject.tag == "minus10pt") {
+			Debug.Log ("I lost 10 points");
+			Destroy (col.gameObject);
+			score -= 10;
+			scoreText.text = "Score: " + score.ToString ();  
+
 		}
-	
 }
-
-
+	void Start (){
+		score = 0;
+	}
 
 }
