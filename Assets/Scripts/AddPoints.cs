493828f9325a9a360736173cd54735c7567dd416
Joshua Iceton <Iceto.J01@elliotthudsoncollege.ac.uk>
2020-09-15 13:36:52 +0100

    End of Task 6
    Co-Authored-By: Andy O'Hara <37961382+compohara@users.noreply.github.com>

--- Assets/Scripts/AddPoints.cs
+++ Assets/Scripts/AddPoints.cs
@@ -6,32 +6,51 @@ using TMPro;
 
 public class AddPoints : MonoBehaviour {
 
-	private int score;
+	public int score;
 	public TextMeshPro scoreText;
+	public TextMeshPro goodText;
+	private GameObject[] collect5ptArray;
+	private int count5pt;
+	public TextMeshPro badText;
+	private GameObject[] collect10ptArray;
+	private int count10pt;
 
-	//void OnCollisionEnter(Collision col) {
-	//	if (col.gameObject.tag == "collectible5pt") {
-	//		Debug.Log ("I collected 5 points");
-	//		Destroy (col.gameObject);
-	//		score += 5;
-	//		scoreText.text = "Score: " + score.ToString ();
+
+
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "minus10pt") {
+			Debug.Log ("I lost 10 points");
+			Destroy (col.gameObject);
+			score -= 10;
+			scoreText.text = "Score: " + score.ToString (); 
+
+			count10pt -= 1;
+			badText.text = "Bad: " + count10pt.ToString ();
+		}
+	}
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "collectible5pt") {
 			Debug.Log ("I collected 5 points");
 			Destroy (col.gameObject);
 			score += 5;
 			scoreText.text = "Score: " + score.ToString ();
-		} else if (col.gameObject.tag == "minus10pt") {
-			Debug.Log ("I lost 10 points");
-			Destroy (col.gameObject);
-			score -= 10;
-			scoreText.text = "Score: " + score.ToString ();  
+			count5pt -= 1;
+			goodText.text = "Good: " + count5pt.ToString ();
+		}	 
 
-		}
 	}
 
 	void Start (){
 		score = 0;
+
+		collect5ptArray = GameObject.FindGameObjectsWithTag ("collectible5pt");
+		count5pt = collect5ptArray.Length;
+		goodText.text = "Good: " + count5pt.ToString ();
+
+		collect10ptArray = GameObject.FindGameObjectsWithTag ("minus10pt");
+		count10pt = collect10ptArray.Length;
+		badText.text = "Bad: " + count10pt.ToString ();
 	}
 
 	
---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckWin : MonoBehaviour {
+
+	private int gameScore;
+		
+	public TextMeshPro timerText;
+	
+
+	void Update () {
+		//finds object named player and then finds script attached to it called AddPoints, then pull out score"
+	gameScore = GameObject.FindWithTag("player").GetComponent<AddPoints>().score;
+		Debug.Log ("Game Score: " + gameScore);
+
+		if (gameScore > 10) {
+			Debug.Log ("WINNER!");
+		}
+		else if (gameScore < 0) {
+			Debug.Log ("LOSER!");
+		}
+		timerText.text = "Time: " + Time.fixedTime.ToString ();
+}
+}
